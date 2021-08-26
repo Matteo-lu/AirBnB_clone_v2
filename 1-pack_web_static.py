9b8e342f2fd214846d6d78840ddd46f2375cb5aa
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-26 12:57:58 -0500

    Uploading task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" script that sets up your web servers for the deployment of web_static """
+
+
+def do_pack():
+    """ script that sets up your web servers for the
+    deployment of web_static """
+
+    from fabric.api import local
+    from datetime import datetime
+    from fabric.context_managers import cd
+    import os.path
+
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d%H%M%S")
+    file_name = 'web_static_' + dt_string
+    source_dir = '/AirBnB_clone_v2/web_static'
+
+    if not os.path.exists('versions'):
+        local("mkdir -p versions")
+    local("tar -zcvf versions/%s.tgz --absolute-names %s" %
+          (file_name, source_dir))
+    path_file = 'versions' + '/' + file_name + '.tgz'
+
+    if os.path.exists(path_file):
+        return(path_file)
+    else:
+        return(None)
