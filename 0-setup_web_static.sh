405f727d964c5fe99c6018c7857c1306ef226da8
Mateo Londoño <mateolondono.u@gmail.com>
2021-08-28 15:33:42 -0500

    Slash correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
-if [ ! -x /usr/sbin/nginx ]; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
-sudo mkdir -p /data/web_static/releases/test/
+sudo apt-get update
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo tee -a /data/web_static/releases/test/index.html > /dev/null << END
 <html >
 <head >
@@ -15,7 +13,7 @@ Holberton School
 </body >
 </html >
 END
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server_name_;/ a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
