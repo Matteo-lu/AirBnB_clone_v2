8c6d6ee6f87578abb0dffbbfae083f8dd5b3c9d6
Matteo-lu <mateolondono.u@gmail.com>
2021-09-12 15:06:56 -0500

    Uploading task files

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+
+from flask import Flask
+app = Flask(__name__)
+app.strict_slashes=False
+
+@app.route('/')
+def hello():
+	return ('Hello HBNB!')
+
+@app.route('/hbnb')
+def hbnb():
+	return ('HBNB')
+
+@app.route('/c/<text>')
+def c_handler(text):
+	new_text = text.replace("_", " ")
+	return ('C ' + new_text)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+
+from flask import Flask
+from flask import abort
+app = Flask(__name__)
+app.strict_slashes=False
+
+@app.route('/')
+def hello():
+	return ('Hello HBNB!')
+
+@app.route('/hbnb')
+def hbnb():
+	return ('HBNB')
+
+@app.route('/c/<text>')
+def c_handler(text):
+	new_text = text.replace("_", " ")
+	return ('C ' + new_text)
+
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_handler(text):
+	new_text = text.replace("_", " ")
+	return ('Python ' + new_text)
+
+@app.route('/number/<int:n>')
+def integer_handler(n):
+	return (str(n) + ' is a number')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
